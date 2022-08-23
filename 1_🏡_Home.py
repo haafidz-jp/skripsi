e2a70e556c567723c0cc4dee63578345fde005ab
haafidz-jp <haafidz123@gmail.com>
2022-08-23 23:26:53 +0700

    rename file

---
+++ 1_🏡_Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+#logo aplikasi MyIndihome
+st.markdown("<center> <img src='https://play-lh.googleusercontent.com/mJwkmmiNzFAtcdbr-otaeNNx4uVOJZ9PqzkG3iJqhy-lzlxUsu_MJ_XOCvTTwW-qK_Y=w240-h480-rw'></img></center>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color: gray;'>Logo Aplikasi MyIndihome</p>", unsafe_allow_html=True)
+
+st.markdown(' # Analisis Sentimen Ulasan Pengguna Aplikasi MyIndihome di Google Play Store dengan menggunakan algoritma K-Nearest Neighbor (KNN) ')
+
+st.markdown('---')
+
+st.text('Nama  : Haafidz Nurul Salim')
+st.text('NPM   : 52418987')
+st.text('Kelas : 4IA23')
+st.text('Prodi : Teknologi Industri')
+st.text('Jurusan : Teknik Informatika')
