9875531c70605e4099878a379be36bc45616bd42
haafidz-jp <haafidz123@gmail.com>
2022-08-24 00:21:34 +0700

    commit

--- pages/4_📈_Analysis Data.py
+++ pages/4_📈_Analysis Data.py
@@ -1,2 +1,14 @@
 import streamlit as st
 
+# menghitung vector
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
+content = df0012['content']
+
+tfidfvectorizer = CountVectorizer(analyzer = 'word')
+tfidf_wm = tfidfvectorizer.fit_transform(content)
+tfidf_tokens = tfidfvectorizer.get_feature_names()
+df_tfidfvect = pd.DataFrame(data = tfidf_wm.toarray(),columns = tfidf_tokens)
+print("\nTD-IDF Vectorizer\n")
+print(df_tfidfvect)
+
